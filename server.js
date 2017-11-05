07e5905057545921ff8132a830c8690d4c989f6b
BolajiOlajide <bolaji.olajide@andela.com>
2017-11-05 19:04:14 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,56 @@
+// server.js
+
+'use strict';
+
+const Hapi = require('hapi');
+const Boom = require('boom');
+const mongoose = require('mongoose');
+const glob = require('glob');
+const path = require('path');
+const secret = require('./config');
+const dotenv = require('dotenv');
+
+dotenv.config()
+
+const server = new Hapi.Server();
+
+const port = process.env.PORT || 8000;
+
+// The connection object takes some
+// configuration, including the port
+server.connection({ port: 5300 });
+
+const dbUrl = process.env.DB_URL;
+
+server.register(require('hapi-auth-jwt'), (err) => {
+
+  // We're giving the strategy both a name
+  // and scheme of 'jwt'
+  server.auth.strategy('jwt', 'jwt', {
+    key: secret,
+    verifyOptions: { algorithms: ['HS256'] }
+  });
+
+  // Look through the routes in
+  // all the subdirectories of API
+  // and create a new route for each
+  glob.sync('api/**/routes/*.js', {
+    root: __dirname
+  }).forEach(file => {
+    const route = require(path.join(__dirname, file));
+    server.route(route);
+  });
+});
+
+// Start the server
+server.start((err) => {
+  if (err) {
+    throw err;
+  }
+  // Once started, connect to Mongo through Mongoose
+  mongoose.connect(dbUrl, {}, (err) => {
+    if (err) {
+      throw err;
+    }
+  });
+});
